5dd8ddbbd02b72275b58593aaa6ec0cab2ffbdac
HudemaDana <dlloved@yahoo.com>
2024-05-09 16:59:02 +0300

    dependencies and api project resolved

--- BApp.API/Program.cs
+++ BApp.API/Program.cs
@@ -1,22 +1,44 @@
+using BApp.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:7219")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+});
+
 // Add services to the container.
+builder.Services.AddDbContext<BAppDbContrext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+//builder.Services.AddScoped<IClientService, ClientService>();
+//builder.Services.AddScoped<ITeamLeaderService, TeamLeaderService>();
+//builder.Services.AddScoped<IUserService, UserService>();
+//builder.Services.AddScoped<ITripService, TripService>();
+
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- BApp.DataAccess/Data/BAppDbContrext.cs
+++ BApp.DataAccess/Data/BAppDbContrext.cs
@@ -4,8 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BApp.DataAccess.Data
 {
-    public class BAppDbContrext: DbContext
+    public class BAppDbContrext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserColor> UserColors { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<UserLevel> UserLevels { get; set; }
+        public DbSet<ExternalColor> ExternalColors { get; set; }
         public BAppDbContrext(DbContextOptions<BAppDbContrext> options) : base(options)
         {
 
@@ -13,16 +18,8 @@ namespace BApp.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Level>().HasData(
-                new Level { 
-                Id = 1,
-                });
         }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserColor> UserColors { get; set; }
-        public DbSet<Level> Levels { get; set; }
-        public DbSet<UserLevel> UserLevels { get; set; }
-        public DbSet<ExternalColor> ExternalColors { get; set; }
+
 
     }
 }
